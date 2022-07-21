7038ce867dfcb20aa70679d672fc6ad268afdfcb
Marvin Alexander Schäcke <86851410+mschaecke@users.noreply.github.com>
2022-07-22 01:05:57 +0200

    Update helloworld.py

--- learn-python/helloworld.py
+++ learn-python/helloworld.py
@@ -1,1 +1,15 @@
-print("Hello, world!")
+# You can open a terminal and type "python3" (or "py")
+# to run Python as a command line
+# OR
+# open a terminal at a directory with .py files
+# and run them via "python3 <file name>"
+
+# Try this code:
+
+print("Hello World!")
+
+#
+# It is also used to create a blank line
+# (invisible newline character or '\n')
+print()
+print("How are you?")
