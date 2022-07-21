5a10862b225926abb2443b22588f58303308a8fb
Marvin Alexander Schäcke <86851410+mschaecke@users.noreply.github.com>
2022-07-22 01:06:06 +0200

    Create comments.py

---
+++ learn-python/comments.py
@@ -0,0 +1,14 @@
+# A simple comment
+
+number = 1  # A comment after code
+
+# A
+# multiline
+# comment
+
+'''
+Another multiline comment
+using string literals
+not assigned to a variable
+(bad practice)
+'''
