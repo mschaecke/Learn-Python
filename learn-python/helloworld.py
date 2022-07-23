2d880638286291b03c05e38489a62b9a5266067a
Marvin Alexander Schäcke <86851410+mschaecke@users.noreply.github.com>
2022-07-23 03:35:04 +0200

    Update helloworld.py

--- learn-python/helloworld.py
+++ learn-python/helloworld.py
@@ -1,3 +1,29 @@
+# Learn-Python by Marvin Alexander Schäcke
+# https://github.com/mschaecke/Learn-Python
+
+# This repository is a collection of Python fundamentals
+# Feel free to use it for your own learning process or to teach others
+
+# If you find any errors or would like to add information, you are
+# very welcome to improve this repository
+
+# Python is a general-purpose programming language
+
+# It offers the following advantages (or disadvantages) compared to other
+# programming languages:
+#   * high-level
+#   * interpreted (instead of compiled)
+#   * multiple programming paradigms (e.g. object-oriented, functional)
+#   * emphasizes readability (similar to the English language)
+#   * uses indentation (instead of brackets)
+#   * dynamically-typed (type checking at runtime)
+#   * garbage collection (instead of manual memory management)
+#   * extensive standard library
+
+# If you do not understand these terms, you will still be able to use this
+# introduction to learn Python
+# We will start with a very simple "Hello World!"
+
 # You can open a terminal and type "python3" (or "py")
 # to run Python as a command line
 # OR
@@ -8,8 +34,11 @@
 
 print("Hello World!")
 
-#
-# It is also used to create a blank line
+# print() is also used to create a blank line
 # (invisible newline character or '\n')
+
 print()
 print("How are you?")
+
+# Read the other .py files to learn the different topics
+# "comments.py" and "variables.py" are both good starting points
