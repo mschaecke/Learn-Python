fd002abdfa2030a82c318ec7269df03a606edd55
Marvin Alexander Schäcke <86851410+mschaecke@users.noreply.github.com>
2022-07-26 20:50:32 +0200

    Create data_types.py

---
+++ learn-python/data_types.py
@@ -0,0 +1,24 @@
+# Variables store different types of data (information)
+# Data types (including primitive types) are classes in Python
+# When a value is assigned to a variable, Python automatically sets the correct
+# data type
+
+# The data type can be checked via the type() function
+
+variable = "Hello World!"
+
+print(variable)
+print(type(variable))
+
+variable = 1
+
+print(variable)
+print(type(variable))
+
+# Alternatively, the data type can be specified via constructor functions
+# (referred to as casting)
+# The constructor functions are shown below each data type
+
+# The following data types will be elaborated in their respective .py files:
+#   * Strings (strings.py)
+#   * Numbers (numbers.py)
