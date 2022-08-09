2bde32a2f284da89f9a2d43defd81fb7ea92cd59
Marvin Alexander Schäcke <86851410+mschaecke@users.noreply.github.com>
2022-08-09 22:15:51 +0200

    Update numbers.py

--- learn-python/numbers.py
+++ learn-python/numbers.py
@@ -129,3 +129,124 @@ print(type(variable))
 # Underscore separators can be replaced by a symbol in f-strings
 variable = 1_000_000
 print(f"{variable:,}")
+
+# Python offers the following seven arithmetic operators to perform
+# mathematical operations:
+
+# Addition "+"
+# Adds two values
+
+variable = 1 + 1  # variable = 2
+print(variable)
+
+# Subtraction "-"
+# Subtracts the second value from the first value
+
+variable = 3 - 2  # variable = 1
+print(variable)
+
+# Multiplication "*"
+# Multiplies two values to find the product
+
+variable = 2 * 3  # variable = 6
+print(variable)
+
+# Division "/"
+# Divides the first operand by the second to find the quotient
+# Division always returns a float
+# Use int() to return an integer
+
+variable = 9 / 3  # variable = 3.0
+print(variable)
+
+variable = int(9 / 3)  # variable = 3
+print(variable)
+
+# Floor division "//"
+# Divides the first operand by the second to find the floor of the quotient
+# (rounding down to the nearest integer)
+
+variable = 7 // 3  # variable = 2
+print(variable)
+
+# Modulus "%"
+# Divides the first operand by the second to find the remainder
+
+variable = 7 % 3  # variable = 1
+print(variable)
+
+# Exponents "**"
+# Raises the first operand to the power of the second (exponent)
+
+variable = 10 ** 3  # variable = 1000
+print(variable)
+
+# The operator precedence is:
+#   brackets
+#   exponents
+#   division
+#   multiplication
+#   floor division
+#   modulus
+#   addition
+#   subtraction
+
+# https://docs.python.org/3/reference/expressions.html#operator-precedence
+
+# Brackets (parentheses) can be used to change the operator precedence:
+
+variable = 3 + 2 * 10  # variable = 23
+print(variable)
+
+variable = (3 + 2) * 10  # variable = 50
+print(variable)
+
+# Additionally, Python offers the following mathematical functions:
+
+# Absolute
+# Returns the absolute value of a number
+
+print(abs(-42))  # Output = 42
+
+# Sum
+# Returns the sum of the function input
+# (function arguments corresponding to the function parameters)
+
+number_list = [1, 2, 3]
+print(sum(number_list))  # Output = 6
+
+# Maximum
+# Returns the largest number
+
+number_list = [7, 42, 1, 23]
+print(max(number_list))  # Output = 42
+
+# Minimum
+# Returns the smallest number
+
+number_list = [7, 42, 1, 23]
+print(min(number_list))  # Output = 1
+
+# Divmod
+# Returns both the quotient and remainder of a division as a tuple
+
+print(divmod(5, 3))  # Output = (1, 2)
+
+print(divmod(5.5, 3))  # Output = (1.0, 2.5)
+
+# Round
+# Returns an integer by rounding a floating-point number
+# Both rounding up and down are possible
+
+print(round(42.89))  # Output = 43
+
+print(round(42.49))  # Output = 42
+
+# Power
+# Raises the first number to the power of the second (exponent)
+
+print(pow(10, 3))  # Output = 1000
+
+# Modulus can be used as well (base, exponent, modulus)
+
+print((pow(10, 3, 3)))  # Output = 1
