dc7f6418f49d9c225b66edaecb29971622fc0b3a
Marvin Alexander Schäcke <86851410+mschaecke@users.noreply.github.com>
2022-08-29 17:22:10 +0200

    Update lists.py

--- learn-python/lists.py
+++ learn-python/lists.py
@@ -73,9 +73,46 @@ print(variable_two)
 variable_two = variable[:4:3]  # variable_two = [1, 4]
 print(variable_two)
 
+# List items can be accessed using loops
+
+variable = [1, 2, 3, 4, 5]
+
+# For loop
+
+for value in variable:
+    print(value)
+
+# For loop with range()
+
+for i in range(2):
+    print(variable[i])  # Prints 1 and 2
+
+# For loop with range() and len()
+
+for i in range(len(variable)):
+    print(variable[i])
+
+# While loop
+
+i = 0
+while i < len(variable):
+    print(variable[i])
+    i += 1
+
+# List comprehension (will be explained in more detail later)
+
+[print(value) for value in variable]
+
+# Reverse a list using reverse()
+
+variable = [1, 2, 3, 4, 5]
+variable.reverse()
+print(variable)
+
 # Reverse the list via -1 step value
 
-variable_two = variable[::-1]  # variable_two = [6, 5, 4, 3, 2, 1]
+variable = [1, 2, 3, 4, 5]
+variable_two = variable[::-1]  # variable_two = [5, 4, 3, 2, 1]
 print(variable_two)
 
 # The length of a list (amount of items) can be returned via the len() function
@@ -178,7 +215,57 @@ print(variable)
 
 del variable  # variable is completely deleted
 
-# TODO: List sorting
+# Lists can be sorted in various ways
+
+variable = [2, 3, 5, 4, 2, 42, 89, 0]
+print(variable)
+variable.sort()
+print(variable)
+
+variable = ["Charlie", "Alice", "Bob"]
+print(variable)
+variable.sort()
+print(variable)
+
+# Sort is case-sensitive (upper before lower case)
+
+variable = ["a", "A", "b", "C", "c"]
+print(variable)
+variable.sort()
+print(variable)  # Prints ['A', 'C', 'a', 'b', 'c']
+
+# Sort can be case-insensitive using key=str.lower
+
+variable = ["c", "A", "a", "C", "b"]
+print(variable)
+variable.sort(key=str.lower)
+print(variable)  # Prints ['A', 'a', 'b', 'c', 'C']
+
+# A descending order is possible as well
+
+variable = [2, 3, 5, 4, 2, 42, 89, 0]
+print(variable)
+variable.sort(reverse=True)
+print(variable)
+
+variable = ["Charlie", "Alice", "Bob"]
+print(variable)
+variable.sort(reverse=True)
+print(variable)
+
+
+# Sort using a custom function
+def even(x):
+    return x % 2 == 0
+
+
+variable = [1, 2, 3, 4, 5]
+variable.sort(key=even)
+print(variable)  # Prints [1, 3, 5, 2, 4]
+
+variable = [1, 2, 3, 4, 5]
+variable.sort(key=even, reverse=True)
+print(variable)  # Prints [2, 4, 1, 3, 5]
 
 # TODO: Other list methods
 
@@ -189,4 +276,58 @@ variable = list((1, 2, "Hello", "World", False))
 print(variable)
 print(type(variable))
 
-# TODO: List comprehension
+# List comprehension
+# Uses the following syntax:
+# variable_name = [expression for item in iterable]
+# Expressions can be the current item or manipulations of the item
+# (e.g. print(x), x.lower() or x * 2)
+# Iterables are objects like lists, tuples, sets, etc.
+# Additionally, one or multiple conditions can be added (condition must be met)
+# variable_name = [expression for item in iterable if condition == True]
+
+# A simple list containing the numbers 0 to 9
+# Can be used instead of for loops
+
+variable = [value for value in range(10)]
+print(variable)
+
+# Only even numbers (0, 2, 4, 6 and 8 in this case)
+
+variable = [value for value in range(10) if value % 2 == 0]
+print(variable)
+
+# Only even numbers using the even() function
+
+variable = [value for value in range(10) if even(value)]
+print(variable)
+
+# Only even numbers > 5 (6 and 8 in this case)
+
+variable = [value for value in range(10) if value % 2 == 0 and value > 5]
+print(variable)
+
+# Print odd numbers separately
+
+[print(value) for value in range(10) if value % 2 == 1]
+
+# Create a new list based on an existing list
+
+variable = [value for value in range(10)]
+new_variable = [x * 2 for x in variable]
+print(new_variable)
+
+variable = ["Alice", "Bob", "Charlie"]
+
+new_variable = [names for names in variable if "i" in names]
+print(new_variable)
+
+variable = [x for x in range(5)]
+new_variable = ["x" for item in variable]  # "x" instead of the number
+print(new_variable)
+
+variable = [x for x in range(10)]
+# Return True if item is even,
+# else return False
+# as new list called new_variable
+new_variable = [True if x % 2 == 0 else False for x in variable]
+print(new_variable)
