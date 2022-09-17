9421944b597e70fd56bb7f6816dadad917df3ccc
Marvin Alexander Schäcke <86851410+mschaecke@users.noreply.github.com>
2022-09-17 17:33:42 +0200

    Create dictionaries.py

---
+++ learn-python/dictionaries.py
@@ -0,0 +1,13 @@
+# TODO: description
+
+# TODO: fundamentals
+
+# TODO: accessing items
+
+# TODO: check for existence
+
+# TODO: modify items (add, change, delete)
+
+# TODO: other methods
+
+# TODO: clear and delete
